3b22d43cdcca600c8142b18c3b7fb5a8c047781e
Dima Bykadorov <dimka.b@live.com>
2023-07-21 17:48:44 +0300

    add Task 04

---
+++ Task_04/Program.cs
@@ -0,0 +1,8 @@
+﻿List <int> numbers = new List <int>();
+int x = 0;
+Console.WriteLine("Введите три числа: ");
+while (x < 3){
+    numbers.Add(int.Parse(Console.ReadLine()));
+    x++;
+}
+Console.WriteLine($"Число c максимальным значением: {numbers.Max()}");
