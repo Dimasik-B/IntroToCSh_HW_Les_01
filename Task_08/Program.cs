cc6c20a6a1342f65cc1603e89fe0defb0373d6aa
Dima Bykadorov <dimka.b@live.com>
2023-07-21 19:17:39 +0300

    add Task 08

---
+++ Task_08/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+string result = null;
+while (count <= number){
+    result += (count % 2 == 0 ? count.ToString() : ", ");
+    count++;
+}
+if (number % 2 != 0) Console.WriteLine(result.Remove(result.Length - 2));
+else Console.WriteLine(result);
+
