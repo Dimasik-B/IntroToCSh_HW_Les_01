bb1bda2ba0b1ef7eeee5fc73cc0914303a876b99
Dima Bykadorov <dimka.b@live.com>
2023-07-21 19:18:15 +0300

    correct problems

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("Введите первое число: ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine()!);
 
 if (a > b) Console.WriteLine($"{a} > {b}");
 else Console.WriteLine($"{b} > {a}");
